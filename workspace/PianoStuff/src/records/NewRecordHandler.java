2871b100235ab124439aabbd62f051be8afa0882
kelvis24 <elviskimara@outlook.com>
2020-03-27 13:18:34 -0500

    updated newrecordhandler with no issues

--- workspace/PianoStuff/src/records/NewRecordHandler.java
+++ workspace/PianoStuff/src/records/NewRecordHandler.java
@@ -1,12 +1,23 @@
 package records;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+
 public class NewRecordHandler {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws LineUnavailableException {
 		
 		AudioFormat format = new AudioFormat(16000, 8, 2, true, true);
 		
-		DataLine.Info info = new DataLine.info(TargetDataLine.class, format);	
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);	
 	
 	
 	    if (!AudioSystem.isLineSupported(info) ) {
@@ -14,18 +25,26 @@ public class NewRecordHandler {
 			
 		}	
 		
-		final TargetDataLine targetDataLine = (TargetDataLine)AudioSystem.getLine(info);
+	//	final TargetDataLine targetDataLine = null;
+		
+	    final TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(info);
+		
+	
 		
 		
-		
-		TargetDataLine.open();
+		try {
+			targetDataLine.open();
+		} catch (LineUnavailableException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		System.out.println("Start recording");
 
-	    TargetDataLine.start();
+	    targetDataLine.start();
 		
 		
-		
+	//    final targetDataLine;
 		
 		
 		Thread stopper = new Thread(new Runnable() {
@@ -38,9 +57,9 @@ public class NewRecordHandler {
 				
 				
 				try {
-					AudioSystem.write(audioStream, AudioFileFormat.type.WAVE, waveFile);
+					AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, waveFile);
 				} 
-				catch (IOexception e) {
+				catch (IOException e) {
 					e.printStackTrace();
 					
 				}	
@@ -55,11 +74,18 @@ public class NewRecordHandler {
 			
 			stopper.start();
 			
-			Thread.sleep(5000);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 			targetDataLine.stop();
 			
-			targetDataLine.close():
+			
+			
+			targetDataLine.close();
 				
 			System.out.println("Ended sound test");	
 				
@@ -71,4 +97,3 @@ public class NewRecordHandler {
 	}
 	
 	
-}
