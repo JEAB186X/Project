96a7d31912e3d0b5f4ef1cf36bbd927b916a779e
kelvis24 <elviskimara@outlook.com>
2020-03-27 12:41:52 -0500

    updating my branch to recent

--- workspace/PianoStuff/src/audio/AudioHandler.java
+++ workspace/PianoStuff/src/audio/AudioHandler.java
@@ -9,60 +9,39 @@ import records.RecordHandler;
 
 public class AudioHandler {
 	
-	public void playFile(int index, String instrument) {
-		
+	RecordHandler rh;
+	
+	public AudioHandler(RecordHandler rh) {
+		this.rh = rh;
+	}
+	
+	public void playFile(int index) {
 		try {
-			
-			
-			
-			File music = new File("/C:/Users/elvis/Documents/Notes/"  + instrument + "/Note" + index + ".wav");
+			// this directory might change depending mine starts with /C:/Users/elvis/Documents/_________rh.instrument() 
+			File music = new File("Notes/"  + "Piano" + "/Note" + index + ".wav");
 			
 			if (music.exists()) {
 				AudioInputStream audioInput = AudioSystem.getAudioInputStream(music);	
 				Clip clip = AudioSystem.getClip();
 				clip.open(audioInput);
 				clip.start();
-				
 			}
 			else {
 				System.out.println("Couldnt find the file");
-				
 			}
 		}
 		catch (Exception ex ) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	//must start playing the current record in the recordhandler class.
 	void startPlayingRecord(RecordHandler rh) {
 
-
-
-
-
-
-
-	
 	}
 	// must stop playing the current record if it is playing.
 	void stopPlayingRecord() {
-		 
-
-
-
-
-
-
-
-		 
-		 
+		
 	 }
 
 }
-	
-	
-	
-	
-	
-	
--- workspace/PianoStuff/src/core/PianoManCore.java
+++ workspace/PianoStuff/src/core/PianoManCore.java
@@ -10,7 +10,8 @@ import audio.AudioHandler;
 
 public class PianoManCore extends JFrame {
 	
-	protected RecordHandler rh;
+	private RecordHandler rh;
+	private AudioHandler ah;
 	
 	protected JToggleButton recordButton;
 	protected JToggleButton playbackButton;
@@ -19,16 +20,16 @@ public class PianoManCore extends JFrame {
 	
 	public PianoManCore() {
 		rh = new RecordHandler();
+		ah = new AudioHandler(rh); 
+		//Initialize according to the data in the Record Handler.
+	}
+	
+	public void initialize() {
+		instrument.setSelectedItem(rh.instrument());
 	}
 	
 	protected void keyPressed(int index) {
-		//Old Code; Replaced
-		
-		String device = (String) instrument.getSelectedItem();
-				
-		AudioHandler musicObject = new AudioHandler(); 
-		musicObject.playFile(index, device);
-		
+		ah.playFile(index);
 	}
 	
 	protected void openPressed() {
@@ -48,17 +49,60 @@ public class PianoManCore extends JFrame {
 	}
 	
 	protected void instrumentSelected() {
-		
+		rh.setInstrument(instrument.getSelectedItem().toString());
 	}
 	
-	protected void rhythmButtonPressed(int value) {
-		
+	protected void rhythmButtonPressed(JPanel[] rhythmNotes, int value) {
+		int i = 0;
+		JPanel p = new JPanel();
+		while (i < 13)
+		{
+			if (value == 1)
+			{
+				ImageIcon a = new ImageIcon("Notes/Images/modifiedWholeNote.jpg");
+				JLabel label = new JLabel(a);
+				p.add(label);
+				rhythmNotes[i] = p;
+			//	rhythmNotes[i] = rhythmNotes[i].add("Notes/Images/modifiedWholeNote.jpg");
+			//	rhythmNotes[i] = new ImageIcon
+			}
+			if (value == 2)
+			{
+				ImageIcon a = new ImageIcon("Notes/Images/modifiedWholeNote.jpg");
+				JLabel label = new JLabel(a);
+				p.add(label);
+				rhythmNotes[i] = p;
+			}
+			if (value == 4)
+			{
+				ImageIcon a = new ImageIcon("Notes/Images/modifiedWholeNote.jpg");
+				JLabel label = new JLabel(a);
+				p.add(label);
+				rhythmNotes[i] = p;
+			}
+			if (value == 8)
+			{
+				ImageIcon a = new ImageIcon("Notes/Images/modifiedWholeNote.jpg");
+				JLabel label = new JLabel(a);
+				p.add(label);
+				rhythmNotes[i] = p;
+			}
+			if (value == 16)
+			{
+				ImageIcon a = new ImageIcon("Notes/Images/modifiedWholeNote.jpg");
+				JLabel label = new JLabel(a);
+				p.add(label);
+				rhythmNotes[i] = p;
+			}
+			i++;
+		}
 	}
 	
 	/*COOL THINGS :D
 		JToggleButton.setSelected(boolan);
-		JToggleButton.isSelected(boolean);
+		JToggleButton.isSelected();
 		JComboBox.getSelectedItem();
+		JComboBox.setSelectedItem();
 	*/
 	
 }
