04cf0468295dae2b89c865777b38c5fb5cbe91ab
JMarchesani12 <jeremy12@iastate.edu>
2020-04-10 16:09:47 +0100

    got pictures to appear, but not dimensioned correctly

--- workspace/PianoStuff/src/core/PianoManCore.java
+++ workspace/PianoStuff/src/core/PianoManCore.java
@@ -3,7 +3,9 @@ package core;
 import records.*;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -66,7 +68,7 @@ public class PianoManCore extends JFrame {
 		rh.setInstrument(instrument.getSelectedItem().toString());
 	}
 	
-	protected void rhythmButtonPressed(JPanel[] rhythmNotes, int[] rhythms, int value) {
+	protected void rhythmButtonPressed(JPanel x, JPanel[] rhythmNotes, int[] rhythms, int value) {
 		int i = 0;
 		ImageIcon a;
 		while (rhythmNotes[i] != null)
@@ -83,7 +85,7 @@ public class PianoManCore extends JFrame {
 			}
 			else if (value == 2)
 			{
-				a = new ImageIcon("Notes/Images/modifiedHalfNote.jpg");
+				a = new ImageIcon("Notes/Images/modifiedHalfNote.png");
 			}
 			else if (value == 4)
 			{
@@ -91,17 +93,21 @@ public class PianoManCore extends JFrame {
 			}
 			else if (value == 8)
 			{
-				a = new ImageIcon("Notes/Images/modifiedeigthNew.jpg");
+				a = new ImageIcon("Notes/Images/modifiedeigthNew.png");
 			}
 			else
 			{
 				a = new ImageIcon("Notes/Images/modifiedSixteenthNote.jpg");
 			}
 			JLabel label = new JLabel(a);
-			label.setLayout(new BorderLayout());
 			rhythmNotes[i] = new JPanel();
-			rhythmNotes[i].add(label);
+			rhythmNotes[i].setLayout(new GridBagLayout());
+			rhythmNotes[i].setBounds((450 + (50 * i)), 165, 50, 100);
+			rhythmNotes[i].setBackground(Color.BLACK);
 			rhythmNotes[i].setVisible(true);
+			rhythmNotes[i].add(label);
+			x.add(rhythmNotes[i]);
+			pack();
 			rhythms[i] = value;
 		}
 	}
